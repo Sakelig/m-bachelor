473ab53129355c06e55f399d4955408cef364748
sakelig <sak.sakelig@gmail.com>
2024-08-02 14:20:08 +0200

    now works fully with postrges and shit pog

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,39 +1,40 @@
 import dbConnect from "../../lib/dbConnect";
-// import User from "../../models/user";
 import jwt from "jsonwebtoken";
-import cookie from "cookie";
 import { setCookie } from "cookies-next";
+import {hashPassword} from "../../lib/utils";
 
 export default async function handler(req, res) {
-    // await dbConnect();
+    const dbClient = await dbConnect();
 
     const { name, email, password } = req.body;
 
     if (req.method === "POST") {
-        // const userExist = await User.findOne({ email });
-        //
-        // if (userExist)
-        //     return res.status(422).json({ message: "Email already in use!" });
-        //
-        // const user = new User({ name, email, password });
-        // await user.save();
-        //
-        // const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
-        //     expiresIn: "1d",
-        // });
+        // checking if user already exists
+        const userExist = await dbClient.query('SELECT email FROM users WHERE email=$1', [email]);
+        const emailResult = userExist.rows[0]
+        if (emailResult){
+            return res.status(422).json({ message: "Email already in use!" });
+        }
 
-        const token = {"wow": 123}
+        // password before storing it
+        let hashedPassword = await hashPassword(password)
 
+        // adds user to db
+        const createdUser = await dbClient.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING users.id', [name, email, hashedPassword])
+        const newUsersId = createdUser.rows[0].id
+        const token = jwt.sign({ userId: newUsersId }, process.env.TOKEN_SECRET, {
+            expiresIn: "1d",
+        });
         setCookie("token", token, {
             req,
             res,
             maxAge: 60 * 60 * 24, // 1 day
             path: "/",
         });
-
-        // res.status(201).json(user);
+        dbClient.end()
         res.status(201).json(token)
     } else {
+        dbClient.end()
         res.status(424).json({ message: "Invalid method!" });
     }
 }
